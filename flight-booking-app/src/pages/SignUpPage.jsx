eac13599d1fe2e6c5eb10874e92968a23d9992f2
toilahao2 <devilnguyen2017@gmail.com>
2024-05-19 00:11:52 +0700

    Update SignUpPage.jsx

--- flight-booking-app/src/pages/SignUpPage.jsx
+++ flight-booking-app/src/pages/SignUpPage.jsx
@@ -1,47 +1,47 @@
 import React from "react";
+
 const Register = () => {
     return (
-        <div className="flex w-full h-screen">
-            <div className="w-full flex items-center justify-center lg:w-1/2">
-                {/*form*/}
-                <div className="bg-white ">
-                    <h1 className="text-xl font-primary justify-center text-center pt-10">
-                        Welcome To ADINA Hotel!{" "}
+        <div className="flex w-full">
+            <div className="w-full h-5/6 p-20 flex items-center justify-center lg:w-1/2">
+                <div className="bg-white w-96 ml-40">
+                    <h1 className="text-2xl font-semibold text-center">
+                        Welcome To TUF Airline
                     </h1>
                     <div className="mt-4 ">
-                        <div className="pb-5">
+                        <div className="pb-2">
                             <lable className="text-medium font-primary font-bold">
-                                First Name
+                                Full name
                             </lable>
                             <input
-                                className="w-full border-2 border-gray-100 rounded-xl p-3 mt-1 bg-transparent"
-                                placeholder="Enter your first name..."
+                                className="w-full border-2 border-gray-100 rounded-xl p-1 mt-1 ml-2 bg-transparent"
+                                placeholder="Enter your full name..."
                             />
                         </div>
-                        <div className="pb-5">
+                        <div className="pb-2">
                             <lable className="text-medium font-primary font-bold">
-                                Last Name
+                                Date Of Birth
                             </lable>
                             <input
-                                className="w-full border-2 border-gray-100 rounded-xl p-3 mt-1 bg-transparent"
-                                placeholder="Enter your last name..."
+                                className="w-full border-2 border-gray-100 rounded-xl p-1 mt-1 ml-2 bg-transparent"
+                                placeholder="Enter your date of birth..."
                             />
                         </div>
-                        <div className="pb-5">
+                        <div className="pb-2">
                             <lable className="text-medium font-primary font-bold">
-                                User Name
+                                Gender
                             </lable>
                             <input
-                                className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                                className="w-full border-2 border-gray-100 rounded-xl p-1 mt-1 ml-2 bg-transparent"
                                 placeholder="Enter your user name..."
                             />
                         </div>
-                        <div className="pb-5">
+                        <div className="pb-2">
                             <lable className="text-medium font-primary font-bold">
-                                Password
+                                Nationality
                             </lable>
                             <input
-                                className="w-full border-2 border-gray-100 rounded-xl p-2 mt-1 bg-transparent"
+                                className="w-full border-2 border-gray-100 rounded-xl p-1 mt-1 ml-2 bg-transparent"
                                 placeholder="Enter your password..."
                             />
                         </div>
@@ -54,28 +54,26 @@ const Register = () => {
                                 placeholder="Enter your email address..."
                             />
                         </div>
-                        <div className="mt-8 flex flex-col gap-y-4">
-                            <button className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-yellow-500 text-white text-lg font-bold font-primary">
-                                Register Now
-                            </button>
-                            <p className="text-center">
-                                Or already have an account
-                            </p>
-                            <button
-                                className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-black text-white text-lg font-bold font-primary"
-                                onClick={() =>
-                                    (window.location.href = "/Login")
-                                }
-                            >
-                                Login
-                            </button>
-                        </div>
+                    </div>
+                    <div className="mt-4 flex flex-col gap-y-2">
+                        <button className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-blue-700 text-white text-base font-bold font-primary">
+                            Register Now
+                        </button>
+                        <p className="text-center">
+                            Or already have an account
+                        </p>
+                        <button
+                            className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-2 rounded-xl bg-blue-500 text-white text-base font-bold font-primary"
+                            onClick={() => (window.location.href = "/Login")}
+                        >
+                            Login
+                        </button>
                     </div>
                 </div>
             </div>
-            <div className="hidden relative lg:flex h-full items-center w-1/2 justify-center bg-yellow-500">
-                <div className="w-60 h-60 bg-gradient-to-tr from-orange-500 tp-yellow-500 rounded-full animate-bounce" />
-                <div className="w-full h-1/2 absolute bottom-0" />
+            <div className="hidden relative lg:flex items-center w-1/2 h-100 justify-center bg-blue-500">
+                <div className="w-60 h-60 bg-gradient-to-tr from-blue-700 tp-blue-500 rounded-full animate-bounce" />
+                <div className="w-full absolute bottom-0" />
             </div>
         </div>
     );
